0279150739013dd271f88e8eca62789430bf40b3
Harshika Santoshi <hksantoshi112.com>
2023-11-17 16:22:32 -0800

    Upload my first project

--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,39 @@ router.post('/add',function(req, res){
   
 });
 
-router.get('/123', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Express' });
 });
+
+router.get('/edit', function(req, res, next) {
+
+  res.render('edit', { title: 'Express' });
+});
+router.get('/edit/:id/',function(req,res,next){
+  const id = req.params.id;
+  console.log(id);
+  const sql = "Select * from Product where productID = ?";
+  connection.get(sql, id, (err, row) => {
+   console.log(row);
+      
+      res.render("edit", { db_rows_array: row });
+   
+
+
+  });
+});
+
+router.get('/display', function(req, res, next) {
+  const sql = "SELECT * FROM Product"
+  connection.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }console.log(rows);
+      res.render("display", { db_rows_array: rows });
+    });
+ 
+});
+
 
 module.exports = router;
 
